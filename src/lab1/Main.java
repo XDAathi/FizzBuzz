73b8d47c2badf9281a79915d52f2635dacdd2ee7
Aathithya Ananth <110254100+XDAathi@users.noreply.github.com>
2025-09-08 10:14:43 -0400

    main java file + acess to fizzbuzz method changed

---
+++ src/lab1/Main.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] temp = new String[100];
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.main(temp);
+
+    }
+
+}
